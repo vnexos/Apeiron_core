0f4e064fbbd07a261137059b865b05bd4d180194
Trần Việt Đăng Quang <tranvietdangquang@gmail.com>
2026-06-07 16:32:24 +0700

    ✨ tm: Làm sạch màn hình

--- bootloader/main.cpp
+++ bootloader/main.cpp
@@ -4,6 +4,7 @@
 extern "C" [[gnu::ms_abi]] EFI_STATUS
 vnexos_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
 {
+  SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
   SystemTable->ConOut->OutputString(SystemTable->ConOut, EFI_TEXT("Hello world!"));
   while (true)
   {
