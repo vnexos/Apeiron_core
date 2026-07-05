d3aab9b1aab37811f5bd3c740a19177b6faaf7be
Trần Việt Đăng Quang <tranvietdangquang@gmail.com>
2026-07-06 00:44:50 +0700

    🛠️ sl: Thêm quy trình ký tệp EFI bằng công cụ VNExos Ám Băng

--- bootloader/src/main.cpp
+++ bootloader/src/main.cpp
@@ -18,7 +18,7 @@ vnexos_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
 {
   (void)ImageHandle;
   SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
-  SystemTable->ConOut->OutputString(SystemTable->ConOut, EFI_TEXT("Hello world!"));
+  SystemTable->ConOut->OutputString(SystemTable->ConOut, EFI_TEXT("Hello world!\r\n"));
   while (true)
   {
     cpu_halt();
