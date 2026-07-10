faa128018b4ee852330536b0c135d6b404cc4b73
Trần Việt Đăng Quang <tranvietdangquang@gmail.com>
2026-07-10 17:34:37 +0700

    🛠️ sl: Đổi qua dùng thư viện EFILib

--- bootloader/src/main.cpp
+++ bootloader/src/main.cpp
@@ -10,18 +10,22 @@
  * @brief Tệp khởi đầu của Bộ nạp khởi động
  */
 #include <cpu.hpp>
-#include <efi.hpp>
+#include <efilib.hpp>
+
+using namespace EFI;
 
 // TODO: Khi làm memory map thì kiếm phân vùng lớn nhất rồi đẩy rác vào cuối và lùi dần từng trang như stack để sau này dễ bề quản lý
 extern "C" [[gnu::ms_abi]] EFI_STATUS
 vnexos_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
 {
-  (void)ImageHandle;
-  SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
-  SystemTable->ConOut->OutputString(SystemTable->ConOut, EFI_TEXT("Hello world!\r\n"));
+  init(ImageHandle, SystemTable);
+
+  printf("Hello world!\n");
+
   while (true)
   {
     cpu_halt();
   }
+
   return EFI_SUCCESS;
 }
