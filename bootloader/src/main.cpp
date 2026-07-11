1eeefc19c327d7d0181ff5182567b7138fe017d0
Trần Việt Đăng Quang <tranvietdangquang@gmail.com>
2026-07-11 17:01:57 +0700

    ✨ tm: Thêm luồng gửi các thông tin cần thiết từ tệp EFI trước sang tệp EFI sau để tránh việc tính toán lặp lại

--- bootloader/src/main.cpp
+++ bootloader/src/main.cpp
@@ -9,6 +9,7 @@
  * @file main.cpp
  * @brief Tệp khởi đầu của Bộ nạp khởi động
  */
+#include <common.hpp>
 #include <cpu.hpp>
 #include <efilib.hpp>
 
@@ -18,9 +19,42 @@ using namespace EFI;
 extern "C" [[gnu::ms_abi]] EFI_STATUS
 vnexos_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
 {
+  EFI_BOOT_SERVICES* bs = SystemTable->BootServices;
   init(ImageHandle, SystemTable);
 
-  printf("Hello world!\n");
+  /* Dùng giao thức ảnh đã tải để lấy các tham số được truyền từ bộ nạp mồi */
+  EFI_LOADED_IMAGE_PROTOCOL* lip;
+  EFI_GUID                   lipGuid = EFI_LOADED_IMAGE_PROTOCOL_GUID;
+
+  EFI_STATUS status = bs->OpenProtocol(
+      ImageHandle,
+      &lipGuid,
+      (void**)&lip,
+      ImageHandle, nullptr,
+      EFI_OPEN_PROTOCOL_GET_PROTOCOL);
+  if (EFI_ERROR(status))
+  {
+    printf("LOI: Khong the mo giao thuc anh da tai\nNhan phim bat ky de thoat...");
+    waitForKey();
+    printf("\n");
+    return status;
+  }
+
+  if (lip->LoadOptionsSize != sizeof(ApeironCommonParameters))
+  {
+    printf("LOI: Tham so truyen vao khong dung (mong doi: %d byte, nhung co: %d byte)\n\
+      Nhan phim bat ky de thoat...",
+           sizeof(ApeironCommonParameters), lip->LoadOptionsSize);
+    waitForKey();
+    printf("\n");
+    return status;
+  }
+
+  ApeironCommonParameters* params = (ApeironCommonParameters*)lip->LoadOptions;
+
+  printf("Test framebuffer: 0x%16x\n", params->framebuffer);
+
+  /* Tính toán vị trí đẹp cho thanh chờ */
 
   while (true)
   {
