5e86113f0732a8e17cecfb918f95b7770f4d9bdb
Trần Việt Đăng Quang <tranvietdangquang@gmail.com>
2026-08-16 21:27:30 +0700

    ✨ tm: Hoàn tất khâu đóng gói tạm thời cho kernel

--- kernel/src/main.cpp
+++ kernel/src/main.cpp
@@ -12,6 +12,12 @@
 #include <cpu.hpp>
 #include <usx.h>
 
+namespace std::structs {
+struct Abc
+{
+};
+} // namespace std::structs
+
 USX_EXPORT_DATA int* a = (int*)0xffab;
 USX_EXPORT_DATA int* b = (int*)0xccdd;
 
@@ -20,10 +26,19 @@ USX_EXPORT_FUNC int add(int a, int b)
   return a + b;
 }
 
-USX_IMPORT("libahihi", int, sub, int, int);
+USX_EXPORT_FUNC int add(int a, int b, int c, volatile std::structs::Abc** const*&& d)
+{
+  (void)d;
+  return a + b + c;
+}
+
+USX_IMPORT("libahihi", int, sub, int a, int b, const volatile unsigned long long***&& c);
+
+uint64_t zero;
 
 void entry()
 {
+  sub(0, 0, nullptr);
 
   while (true)
   {
